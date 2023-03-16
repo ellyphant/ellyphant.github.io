130ea49c6da56fd81a99bcdc4374cffe96c2ad08
Elly <linelly3@gmail.com>
2023-03-15 18:16:31 -0700

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,3 +1,3 @@
-// importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
 
-importScripts('https://3bca-50-216-35-194.ngrok.io/sdks/Dev-OneSignalSDKWorker.js');
+// importScripts('https://3bca-50-216-35-194.ngrok.io/sdks/Dev-OneSignalSDKWorker.js');
